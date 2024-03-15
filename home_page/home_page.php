73924b68661cd1785dc469d7208779e56e2a6146
kenzo-b <contact@kbriche.fr>
2024-03-15 10:46:53 +0100

    new file and folder for the home_page

---
+++ home_page/home_page.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="inscription.css">
+    <link rel="stylesheet" href="../popup.css">
+    <link rel="icon" type="image/x-icon" href="../LMEV Motif/mot3_cranevache.png">
+    <title>Inscription</title>
+</head>
+
